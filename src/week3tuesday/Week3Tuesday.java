65d47b5ec13b1a8ec74687eb2775cb092bdc5e68
Pratyancha26 <66335019+Pratyancha26@users.noreply.github.com>
2020-06-09 18:02:51 -0400

    Update Week3Tuesday.java

--- src/week3tuesday/Week3Tuesday.java
+++ src/week3tuesday/Week3Tuesday.java
@@ -18,6 +18,7 @@ public class Week3Tuesday {
         // TODO code application logic here
         System.out.println("Learning Push Operation");
         System.out.println("Learning Push Operation + fetch");
+        System.out.println("Learning fetch");
     }
     
 }
